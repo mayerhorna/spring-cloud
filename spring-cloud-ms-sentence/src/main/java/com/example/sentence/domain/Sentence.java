40e8b833a19d34bb52084444121035b0fc7b5825
Mayer Horna <mayer.horna@gmail.com>
2022-07-26 20:57:25 -0500

    avance hasta gateway

--- spring-cloud-ms-sentence/src/main/java/com/example/sentence/domain/Sentence.java
+++ spring-cloud-ms-sentence/src/main/java/com/example/sentence/domain/Sentence.java
@@ -3,6 +3,7 @@ package com.example.sentence.domain;
 import java.io.Serializable;
 
 public class Sentence implements Serializable{
+	private static final long serialVersionUID = 1L;
 	private Long id;
 	private String text;
 	public Long getId() {
